a0dac9ac62b1baf081b92c03f8db338ce3b73f8e
danjlee <danjlee0515@gmail.com>
2020-04-20 21:32:29 -0400

    fix import validations to valid-text

--- validation/login.js
+++ validation/login.js
@@ -1,5 +1,5 @@
 const Validator = require("validator");
-const validText = require("./validations");
+const validText = require("./valid-text");
 
 module.exports = function validateLoginInput(data) {
     let errors = {};
