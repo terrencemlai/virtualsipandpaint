f8704bce0b3e98d59b09028f83f8792755a40167
App Academy Student <email@email.com>
2020-04-22 10:00:54 -0400

    prep for heroku

--- app.js
+++ app.js
@@ -7,11 +7,9 @@ const rooms = require("./routes/api/rooms");
 const User = require("./models/User");
 const bodyParser = require("body-parser");
 const passport = require('passport');
+const path = require('path');
 
-mongoose
-  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Connected to mongoDB"))
-  .catch(err => console.log(err));
+
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -19,16 +17,29 @@ app.use(bodyParser.json());
 app.use(passport.initialize());
 require("./passport/passport")(passport);
 
-app.get("/", (req, res) => {
-  console.log(res);
-  res.send("Welcome to Virtual Sip & Paint!");
-});
+// app.get("/", (req, res) => {
+//   console.log(res);
+//   res.send("Welcome to Virtual Sip & Paint!");
+// });
 
 app.use("/api/users", users);
 app.use("/api/rooms", rooms);
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to mongoDB"))
+  .catch(err => console.log(err));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 const port = process.env.PORT || 5000; 
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
 });
+
