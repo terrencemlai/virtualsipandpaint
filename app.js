2bab6d57de81e2988714672559387088c4479435
your name here <pqpq.bkbk3t3@gmail.com>
2020-05-27 13:53:46 -0400

    fixed routing

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ if (process.env.NODE_ENV === 'production') {
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
+  app.get("*", (req, res) => res.redirect("/")); 
 }
 
 io.on("connection", (socket) => {
